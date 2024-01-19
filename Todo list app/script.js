5a670589766d409361fea98a3c5423097cb354e0
Wasii ullah <143061079+Wasookhan@users.noreply.github.com>
2024-01-19 10:34:10 +0000

    JAVASCRIPT

---
+++ Todo list app/script.js
@@ -0,0 +1,30 @@
+
+const input = document.querySelector("#input");
+const addTask = document.querySelector("#addTask");
+let listContainer = document.querySelector(".list-container");
+
+addTask.addEventListener("click", () => {
+  if (input.value === "") {
+    alert("Please enter a task");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = input.value;
+    listContainer.appendChild(li);
+    input.value = "";
+    saveData();
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.classList.add("Delete");
+    li.appendChild(deleteBtn);
+    deleteBtn.addEventListener("click", () => {
+      li.remove();
+      showData();
+    });
+  }
+});
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showData() {
+  localStorage.getItem("data");
+}
